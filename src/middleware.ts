354ae5e966a8e1d00d67702be6ce6e008c2537d7
Jamal <jm.jamal@raiyaainfotech.com>
2026-04-24 17:50:49 +0530

    update that preview public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const publicRoutes = ['/login', '/forgot-password', '/reset-password'];
+const publicRoutes = ['/login', '/forgot-password', '/reset-password', '/preview'];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
